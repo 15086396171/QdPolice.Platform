26c1f91f8f1f34271693ba93f71272dc8db0dd06
darkyx <845243610@qq.com>
2017-01-17 21:59:01 +0800

    1.添加代码生成器所需文件

---
+++ Vickn.Platform.Application/Dtos/PagedAndSortedInputDto.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services.Dto;
+
+namespace Vickn.Platform.Dtos
+{
+    /// <summary>
+    /// 表示分页排序的Dto基类
+    /// </summary>
+    public class PagedAndSortedInputDto : IPagedResultRequest, ISortedResultRequest
+    {
+        /// <summary>
+        /// Sorting information.
+        ///             Should include sorting field and optionally a direction (ASC or DESC)
+        ///             Can contain more than one field separated by comma (,).
+        /// </summary>
+        /// <example>
+        /// Examples:
+        ///             "Name"
+        ///             "Name DESC"
+        ///             "Name ASC, Age DESC"
+        /// </example>
+        public string Sorting { get; set; }
+
+        /// <summary>
+        /// 初始化分页排序的Dto
+        /// </summary>
+        public PagedAndSortedInputDto()
+        {
+            MaxResultCount = PlatformConsts.DefaultPageSize;
+        }
+
+        /// <summary>
+        /// Max expected result count.
+        /// </summary>
+        [Range(1, PlatformConsts.MaxPageSize)]
+        public int MaxResultCount { get; set; }
+
+        /// <summary>
+        /// Skip count (beginning of the page).
+        /// </summary>
+        [Range(0, int.MaxValue)]
+        public int SkipCount { get; set; }
+
+
+    }
+}
--- Vickn.Platform.Core/PlatformConsts.cs
+++ Vickn.Platform.Core/PlatformConsts.cs
@@ -6,5 +6,49 @@
         //public const string LocalizationSourceName = "Platform-es";
 
         public const string AppName = "Vickn统一登录管理平台";
+        public const int DefaultPageSize = 15;
+        public const int MaxPageSize = 100;
+
+        /// <summary>
+        /// 显示名称的最大长度
+        /// </summary>
+        public const int MaxDisplayNameLength = 64;
+
+        /// <summary>
+        /// 用户名的最大长度
+        /// </summary>
+        public const int MaxUserNameLength = 32;
+
+        /// <summary>
+        /// 数据库架构名
+        /// </summary>
+        public static class SchemaName
+        {
+            /// <summary>
+            /// 基础设置
+            /// </summary>
+            public const string Basic = "Basic";
+
+            /// <summary>
+            /// 模块管理
+            /// </summary>
+            public const string Moudle = "Moudle";
+
+            /// <summary>
+            /// 网站设置
+            /// </summary>
+            public const string WebSetting = "WebSetting";
+            /// <summary>
+            /// 用于对多对表关系的结构
+            /// </summary>
+            public const string HasMany = "HasMany";
+
+            /// <summary>
+            /// 业务
+            /// </summary>
+            public const string Business = "Business";
+
+        }
+
     }
 }
