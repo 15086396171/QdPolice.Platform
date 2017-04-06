aa9237e5b8544735052efa787f3bc16ebdc22adb
dark_yx <845243610@qq.com>
2017-04-06 16:40:59 +0800

    1.修改默认abp为sys2 2.修改默认权限

--- Vickn.Platform.EntityFramework/EntityFramework/PlatformDbContext.cs
+++ Vickn.Platform.EntityFramework/EntityFramework/PlatformDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Common;
+using System.Data.Entity;
 using Abp.Zero.EntityFramework;
 using Vickn.Platform.Authorization.Roles;
 using Vickn.Platform.MultiTenancy;
@@ -36,6 +37,17 @@ namespace Vickn.Platform.EntityFramework
             : base(connection, true)
         {
 
+        }
+
+        /// <summary>
+        /// 在完成对派生上下文的模型的初始化后，并在该模型已锁定并用于初始化上下文之前，将调用此方法。虽然此方法的默认实现不执行任何操作，但可在派生类中重写此方法，这样便能在锁定模型之前对其进行进一步的配置。
+        /// </summary>
+        /// <param name="modelBuilder">定义要创建的上下文的模型的生成器。</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.HasDefaultSchema("basic");
+            modelBuilder.ChangeAbpTablePrefix<Tenant,Role,User>("Sys2");
         }
     }
 }
