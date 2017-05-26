1f6e40226f6b7f5774dea6ab579c7e39c4aa9f65
dark_yx <yangxuilyx@vickn.com>
2017-05-26 21:14:33 +0800

    1.添加enum扩展

---
+++ Vickn.PlatfForm.Utils/Extensions/EnumExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Vickn.PlatfForm.Utils.Extensions
+{
+    /// <summary>
+    /// 自定义属性 显示名称
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public sealed class EnumDescriptionAttribute : Attribute
+    {
+        public string Description { get; }
+
+        public EnumDescriptionAttribute(string description)
+            : base()
+        {
+            this.Description = description;
+        }
+    }
+
+    /// <summary>
+    /// 更具
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        ///  获取枚举字符串 显示名称
+        /// </summary>
+        /// <param name="value">枚举值</param>
+        /// <returns></returns>
+        public static string GetDescription(this System.Enum value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentException("value");
+            }
+            string description = value.ToString();
+            var fieldInfo = value.GetType().GetField(description);
+            var attributes =
+                (EnumDescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(EnumDescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                description = attributes[0].Description;
+            }
+            return description;
+        }
+    }
+}
