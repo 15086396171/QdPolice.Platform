a84901bbebb58516491696884ab2a4b9d4b18246
dark_yx <yangxuilyx@vickn.com>
2017-06-07 17:01:38 +0800

    1.添加默认语言设置接口

---
+++ Vickn.Platform.Application/Zero/Sessions/Dto/CultureInput.cs
@@ -0,0 +1,10 @@
+﻿namespace Vickn.Platform.Sessions.Dto
+{
+    public class CultureInput
+    {
+        /// <summary>
+        /// 语言名称
+        /// </summary>
+        public string CultureName { get; set; } 
+    }
+}
