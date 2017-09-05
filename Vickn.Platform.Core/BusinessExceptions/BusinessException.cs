be645ae89bd4bd9f55eb34f5ad15a9e85a51c3e2
dark_yx-i <845243610@qq.com>
2017-09-05 17:50:30 +0800

    1.添加业务异常

---
+++ Vickn.Platform.Core/BusinessExceptions/BusinessException.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Abp;
+using Abp.Logging;
+using Abp.UI;
+using Vickn.PlatfForm.Utils.Extensions;
+
+namespace Vickn.Platform.BusinessExceptions
+{
+    /// <summary>
+    /// 业务异常
+    /// </summary>
+    public class BusinessException : UserFriendlyException
+    {
+        //
+        // 摘要:
+        //     Constructor.
+        public BusinessException() : base() { }
+        //
+        // 摘要:
+        //     Constructor.
+        //
+        // 参数:
+        //   message:
+        //     Exception message
+        public BusinessException(string message) : base(message) { }
+        //
+        // 摘要:
+        //     Constructor for serializing.
+        public BusinessException(SerializationInfo serializationInfo, StreamingContext context) : base(serializationInfo, context) { }
+        //
+        // 摘要:
+        //     Constructor.
+        //
+        // 参数:
+        //   message:
+        //     Exception message
+        //
+        //   severity:
+        //     Exception severity
+        public BusinessException(string message, LogSeverity severity) : base(message, severity) { }
+
+        public BusinessException(ErrorCode code) : base((int)code, code.GetDescription()) { }
+
+        //
+        // 摘要:
+        //     Constructor.
+        //
+        // 参数:
+        //   code:
+        //     Error code
+        //
+        //   message:
+        //     Exception message
+        public BusinessException(ErrorCode code, string message) : base((int)code, message) { }
+
+        public BusinessException(ErrorCode code, string message, string details) : base((int)code, message, details) { }
+        //
+        // 摘要:
+        //     Constructor.
+        //
+        // 参数:
+        //   message:
+        //     Exception message
+        //
+        //   details:
+        //     Additional information about the exception
+        //
+        //   innerException:
+        //     Inner exception
+        public BusinessException(string message, string details, Exception innerException) : base(message, details, innerException) { }
+    }
+}
---
+++ Vickn.Platform.Core/BusinessExceptions/ErrorCode.cs
@@ -0,0 +1,15 @@
+﻿using Vickn.PlatfForm.Utils.Extensions;
+
+namespace Vickn.Platform.BusinessExceptions
+{
+    /// <summary>
+    /// 错误码定义
+    /// </summary>
+    public enum ErrorCode:int
+    {
+        [EnumDescription("输入错误消息")]
+        Normal=1,
+        #region 注册模块 10000
+        #endregion
+    }
+}
