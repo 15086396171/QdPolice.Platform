2d4916051594795776d026cced8af38bf1a836b2
dark_yx-i <845243610@qq.com>
2018-02-05 15:45:36 +0800

    添加警务通在线状态查看

--- Vickn.Platform.Application/HandheldTerminals/AppWhiteLists/AppWhiteListAppService.cs
+++ Vickn.Platform.Application/HandheldTerminals/AppWhiteLists/AppWhiteListAppService.cs
@@ -32,19 +32,19 @@ using Vickn.Platform.HandheldTerminals.AppWhiteLists.Dtos;
 
 namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
 {
-	/// <summary>
+    /// <summary>
     /// 应用白名单服务
     /// </summary>
-	[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList)]
+    [AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList)]
     public class AppWhiteListAppService : PlatformAppServiceBase, IAppWhiteListAppService
     {
-	    private readonly IRepository<AppWhiteList,long> _appWhiteListRepository;
-	  	private readonly AppWhiteListManager _appWhiteListManager;
+        private readonly IRepository<AppWhiteList, long> _appWhiteListRepository;
+        private readonly AppWhiteListManager _appWhiteListManager;
 
-	    /// <summary>
+        /// <summary>
         /// 初始化应用白名单服务实例
         /// </summary>
-        public AppWhiteListAppService(IRepository<AppWhiteList, long> appWhiteListRepository,AppWhiteListManager appWhiteListManager)
+        public AppWhiteListAppService(IRepository<AppWhiteList, long> appWhiteListRepository, AppWhiteListManager appWhiteListManager)
         {
             _appWhiteListRepository = appWhiteListRepository;
             _appWhiteListManager = appWhiteListManager;
@@ -52,14 +52,17 @@ namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
 
         #region 应用白名单管理
 
-		/// <summary>
+        /// <summary>
         /// 根据查询条件获取应用白名单分页列表
         /// </summary>
         public async Task<PagedResultDto<AppWhiteListDto>> GetPagedAsync(GetAppWhiteListInput input)
-		{
-			 var query = _appWhiteListRepository.GetAll();
+        {
+            var query = _appWhiteListRepository.GetAll();
 
             //TODO:根据传入的参数添加过滤条件
+
+            query = query.WhereIf(!input.FilterText.IsNullOrEmpty(), p => p.Name.Contains(input.FilterText) ||
+                                                                          p.PackageName.Contains(input.FilterText));
 
             var appWhiteListCount = await query.CountAsync();
 
@@ -73,24 +76,24 @@ namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
             appWhiteListCount,
             appWhiteListDtos
             );
-		}
+        }
 
-		/// <summary>
+        /// <summary>
         /// 通过指定id获取应用白名单Dto信息
         /// </summary>
         public async Task<AppWhiteListDto> GetByIdAsync(EntityDto<long> input)
-		{
-		    var entity = await _appWhiteListRepository.GetAsync(input.Id);
+        {
+            var entity = await _appWhiteListRepository.GetAsync(input.Id);
             return entity.MapTo<AppWhiteListDto>();
-		}
+        }
 
         /// <summary>
         /// 通过Id获取应用白名单信息进行编辑或修改
         /// Id为空时返回新对象 
         /// </summary>
         public async Task<AppWhiteListForEdit> GetForEditAsync(NullableIdDto<long> input)
-		{
-			AppWhiteListEditDto appWhiteListEditDto;
+        {
+            AppWhiteListEditDto appWhiteListEditDto;
 
             if (input.Id.HasValue)
             {
@@ -102,15 +105,15 @@ namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
                 appWhiteListEditDto = new AppWhiteListEditDto();
             }
             return new AppWhiteListForEdit { AppWhiteListEditDto = appWhiteListEditDto };
-		}
+        }
 
         /// <summary>
         /// 新增或更改应用白名单
         /// </summary>
-		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_CreateAppWhiteList,AppWhiteListAppPermissions.AppWhiteList_EditAppWhiteList)]
+		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_CreateAppWhiteList, AppWhiteListAppPermissions.AppWhiteList_EditAppWhiteList)]
         public async Task CreateOrUpdateAsync(AppWhiteListForEdit input)
-		{
-			 if (input.AppWhiteListEditDto.Id.HasValue)
+        {
+            if (input.AppWhiteListEditDto.Id.HasValue)
             {
                 await UpdateAsync(input);
             }
@@ -118,57 +121,57 @@ namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
             {
                 await CreateAsync(input);
             }
-		}
+        }
 
         /// <summary>
         /// 新增应用白名单
         /// </summary>
 		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_CreateAppWhiteList)]
         public async Task<AppWhiteListForEdit> CreateAsync(AppWhiteListForEdit input)
-		{
-			//TODO: 新增前的逻辑判断，是否允许新增
+        {
+            //TODO: 新增前的逻辑判断，是否允许新增
 
             var entity = input.AppWhiteListEditDto.MapTo<AppWhiteList>();
 
             entity = await _appWhiteListRepository.InsertAsync(entity);
             return new AppWhiteListForEdit { AppWhiteListEditDto = entity.MapTo<AppWhiteListEditDto>() };
-		}
+        }
 
         /// <summary>
         /// 修改应用白名单
         /// </summary>
 		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_EditAppWhiteList)]
         public async Task UpdateAsync(AppWhiteListForEdit input)
-		{
-		    //TODO: 更新前的逻辑判断，是否允许更新
+        {
+            //TODO: 更新前的逻辑判断，是否允许更新
 
-			var entity = await _appWhiteListRepository.GetAsync(input.AppWhiteListEditDto.Id.Value);
+            var entity = await _appWhiteListRepository.GetAsync(input.AppWhiteListEditDto.Id.Value);
             input.AppWhiteListEditDto.MapTo(entity);
 
             await _appWhiteListRepository.UpdateAsync(entity);
-		}
+        }
 
         /// <summary>
         /// 删除应用白名单
         /// </summary>
 		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_DeleteAppWhiteList)]
         public async Task DeleteAsync(EntityDto<long> input)
-		{
-			//TODO: 删除前的逻辑判断，是否允许删除
+        {
+            //TODO: 删除前的逻辑判断，是否允许删除
 
             await _appWhiteListRepository.DeleteAsync(input.Id);
-		}
+        }
 
         /// <summary>
         /// 批量删除应用白名单
         /// </summary>
 		[AbpAuthorize(AppWhiteListAppPermissions.AppWhiteList_DeleteAppWhiteList)]
         public async Task BatchDeleteAsync(List<long> input)
-		{
-		    //TODO: 批量删除前的逻辑判断，是否允许删除
+        {
+            //TODO: 批量删除前的逻辑判断，是否允许删除
 
             await _appWhiteListRepository.DeleteAsync(s => input.Contains(s.Id));
-		}
+        }
 
         /// <summary>
         /// 自定义检查应用白名单输入逻辑错误
@@ -176,11 +179,11 @@ namespace Vickn.Platform.HandheldTerminals.AppWhiteLists
         /// <param name="input"></param>
         /// <returns></returns>
         public async Task<CustomerModelStateValidationDto> CheckErrorAsync(AppWhiteListForEdit input)
-		{	
-			//TODO: 自定义逻辑判断是否有逻辑错误
+        {
+            //TODO: 自定义逻辑判断是否有逻辑错误
 
-			return new CustomerModelStateValidationDto() {HasModelError = false};
-		}
+            return new CustomerModelStateValidationDto() { HasModelError = false };
+        }
 
         #endregion
 
--- Vickn.Platform.Application/HandheldTerminals/Devices/DeviceAppService.cs
+++ Vickn.Platform.Application/HandheldTerminals/Devices/DeviceAppService.cs
@@ -25,44 +25,45 @@ using Abp.Configuration;
 using Abp.Domain.Repositories;
 using Abp.Extensions;
 using Abp.Linq.Extensions;
-
+using Abp.RealTime;
 using Vickn.Platform.Dtos;
 using Vickn.Platform.HandheldTerminals.Devices.Authorization;
 using Vickn.Platform.HandheldTerminals.Devices.Dtos;
 
 namespace Vickn.Platform.HandheldTerminals.Devices
 {
-	/// <summary>
+    /// <summary>
     /// 设备服务
     /// </summary>
-	[AbpAuthorize(DeviceAppPermissions.Device)]
+    [AbpAuthorize(DeviceAppPermissions.Device)]
     public class DeviceAppService : PlatformAppServiceBase, IDeviceAppService
     {
-	    private readonly IRepository<Device,long> _deviceRepository;
-	  	private readonly DeviceManager _deviceManager;
-
-	    /// <summary>
+        private readonly IRepository<Device, long> _deviceRepository;
+        private readonly DeviceManager _deviceManager;
+        private readonly IOnlineClientManager _onlineClientManager;
+        /// <summary>
         /// 初始化设备服务实例
         /// </summary>
-        public DeviceAppService(IRepository<Device, long> deviceRepository,DeviceManager deviceManager)
+        public DeviceAppService(IRepository<Device, long> deviceRepository, DeviceManager deviceManager, IOnlineClientManager onlineClientManager)
         {
             _deviceRepository = deviceRepository;
             _deviceManager = deviceManager;
+            _onlineClientManager = onlineClientManager;
         }
 
         #region 设备管理
 
-		/// <summary>
+        /// <summary>
         /// 根据查询条件获取设备分页列表
         /// </summary>
         public async Task<PagedResultDto<DeviceDto>> GetPagedAsync(GetDeviceInput input)
-		{
-			 var query = _deviceRepository.GetAll();
+        {
+            var query = _deviceRepository.GetAll();
 
             //TODO:根据传入的参数添加过滤条件
-		    query = query.WhereIf(!input.FilterText.IsNullOrEmpty(), p => p.Imei.Contains(input.FilterText) ||
-		                                                                  p.User.Name.Contains(input.FilterText) ||
-		                                                                  p.User.PoliceNo.Contains(input.FilterText));
+            query = query.WhereIf(!input.FilterText.IsNullOrEmpty(), p => p.Imei.Contains(input.FilterText) ||
+                                                                          p.User.Name.Contains(input.FilterText) ||
+                                                                          p.User.PoliceNo.Contains(input.FilterText));
 
             var deviceCount = await query.CountAsync();
 
@@ -72,28 +73,37 @@ namespace Vickn.Platform.HandheldTerminals.Devices
             .ToListAsync();
 
             var deviceDtos = devices.MapTo<List<DeviceDto>>();
+
+            foreach (var deviceDto in deviceDtos)
+            {
+                if (_onlineClientManager.GetAllByUserId(new UserIdentifier(AbpSession.TenantId, deviceDto.User.Id)).Any())
+                {
+                    deviceDto.IsOnline = true;
+                }
+            }
+
             return new PagedResultDto<DeviceDto>(
             deviceCount,
             deviceDtos
             );
-		}
+        }
 
-		/// <summary>
+        /// <summary>
         /// 通过指定id获取设备Dto信息
         /// </summary>
         public async Task<DeviceDto> GetByIdAsync(EntityDto<long> input)
-		{
-		    var entity = await _deviceRepository.GetAsync(input.Id);
+        {
+            var entity = await _deviceRepository.GetAsync(input.Id);
             return entity.MapTo<DeviceDto>();
-		}
+        }
 
         /// <summary>
         /// 通过Id获取设备信息进行编辑或修改
         /// Id为空时返回新对象 
         /// </summary>
         public async Task<DeviceForEdit> GetForEditAsync(NullableIdDto<long> input)
-		{
-			DeviceEditDto deviceEditDto;
+        {
+            DeviceEditDto deviceEditDto;
 
             if (input.Id.HasValue)
             {
@@ -105,15 +115,15 @@ namespace Vickn.Platform.HandheldTerminals.Devices
                 deviceEditDto = new DeviceEditDto();
             }
             return new DeviceForEdit { DeviceEditDto = deviceEditDto };
-		}
+        }
 
         /// <summary>
         /// 新增或更改设备
         /// </summary>
-		[AbpAuthorize(DeviceAppPermissions.Device_CreateDevice,DeviceAppPermissions.Device_EditDevice)]
+		[AbpAuthorize(DeviceAppPermissions.Device_CreateDevice, DeviceAppPermissions.Device_EditDevice)]
         public async Task CreateOrUpdateAsync(DeviceForEdit input)
-		{
-			 if (input.DeviceEditDto.Id.HasValue)
+        {
+            if (input.DeviceEditDto.Id.HasValue)
             {
                 await UpdateAsync(input);
             }
@@ -121,57 +131,57 @@ namespace Vickn.Platform.HandheldTerminals.Devices
             {
                 await CreateAsync(input);
             }
-		}
+        }
 
         /// <summary>
         /// 新增设备
         /// </summary>
 		[AbpAuthorize(DeviceAppPermissions.Device_CreateDevice)]
         public async Task<DeviceForEdit> CreateAsync(DeviceForEdit input)
-		{
-			//TODO: 新增前的逻辑判断，是否允许新增
+        {
+            //TODO: 新增前的逻辑判断，是否允许新增
 
             var entity = input.DeviceEditDto.MapTo<Device>();
 
             entity = await _deviceRepository.InsertAsync(entity);
             return new DeviceForEdit { DeviceEditDto = entity.MapTo<DeviceEditDto>() };
-		}
+        }
 
         /// <summary>
         /// 修改设备
         /// </summary>
 		[AbpAuthorize(DeviceAppPermissions.Device_EditDevice)]
         public async Task UpdateAsync(DeviceForEdit input)
-		{
-		    //TODO: 更新前的逻辑判断，是否允许更新
+        {
+            //TODO: 更新前的逻辑判断，是否允许更新
 
-			var entity = await _deviceRepository.GetAsync(input.DeviceEditDto.Id.Value);
+            var entity = await _deviceRepository.GetAsync(input.DeviceEditDto.Id.Value);
             input.DeviceEditDto.MapTo(entity);
 
             await _deviceRepository.UpdateAsync(entity);
-		}
+        }
 
         /// <summary>
         /// 删除设备
         /// </summary>
 		[AbpAuthorize(DeviceAppPermissions.Device_DeleteDevice)]
         public async Task DeleteAsync(EntityDto<long> input)
-		{
-			//TODO: 删除前的逻辑判断，是否允许删除
+        {
+            //TODO: 删除前的逻辑判断，是否允许删除
 
             await _deviceRepository.DeleteAsync(input.Id);
-		}
+        }
 
         /// <summary>
         /// 批量删除设备
         /// </summary>
 		[AbpAuthorize(DeviceAppPermissions.Device_DeleteDevice)]
         public async Task BatchDeleteAsync(List<long> input)
-		{
-		    //TODO: 批量删除前的逻辑判断，是否允许删除
+        {
+            //TODO: 批量删除前的逻辑判断，是否允许删除
 
             await _deviceRepository.DeleteAsync(s => input.Contains(s.Id));
-		}
+        }
 
         /// <summary>
         /// 自定义检查设备输入逻辑错误
@@ -179,11 +189,11 @@ namespace Vickn.Platform.HandheldTerminals.Devices
         /// <param name="input"></param>
         /// <returns></returns>
         public async Task<CustomerModelStateValidationDto> CheckErrorAsync(DeviceForEdit input)
-		{	
-			//TODO: 自定义逻辑判断是否有逻辑错误
+        {
+            //TODO: 自定义逻辑判断是否有逻辑错误
 
-			return new CustomerModelStateValidationDto() {HasModelError = false};
-		}
+            return new CustomerModelStateValidationDto() { HasModelError = false };
+        }
 
         #endregion
 
