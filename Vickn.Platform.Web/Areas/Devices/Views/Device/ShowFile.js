9e45d62a4f646ebc15ae7edab7a7282819df513a
dark_yx-i <845243610@qq.com>
2018-02-24 09:42:40 +0800

    文件标签

---
+++ Vickn.Platform.Web/Areas/Devices/Views/Device/ShowFile.js
@@ -0,0 +1,14 @@
+﻿(function() {
+  $(function () {
+    var dataDictionaryService = abp.services.app.dataDictionary;
+
+    dataDictionaryService.getDataDictionaryItemsByDicName({
+      dicKey:"Forensics.Tag"
+    }).done(function(result) {
+      $.each(result.items,function(index, data) {
+        console.log(index, data);
+        $("#tag").append(' <li><a href= "javascript:;" data-value="'+data.value+'">' + data.displayName + '</a></li>');
+      })
+    })
+  });
+})()
