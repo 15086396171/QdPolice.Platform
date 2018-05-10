083c6ece01ea410ac39d336130a8afdca3261e86
赵耀 <@DESKTOP-BL1O324>
2018-05-10 17:35:39 +0800

    添加app时间接口

---
+++ Vickn.Platform.Application/PbManagement/PositionPbTimes/Dtos/AppGetPositionPbDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vickn.Platform.PbManagement.PositionPbTimes.Dtos
+{
+    /// <summary>
+    /// app调用后台获取排班信息接口参数
+    /// </summary>
+   public class AppGetPositionPbDto
+    {
+        /// <summary>
+        ///排班日期（年月日）
+        /// </summary>
+        public DateTime PbDate{ get; set; }
+    }
+}
