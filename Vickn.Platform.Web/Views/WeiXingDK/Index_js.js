d03880ac237b7a05eec7aa5f76aa48bee3ab2117
15086396171 <15086396171@163.com>
2019-01-04 16:05:30 +0800

    高德地图禁用城市ip定位

--- Vickn.Platform.Web/Views/WeiXingDK/Index_js.js
+++ Vickn.Platform.Web/Views/WeiXingDK/Index_js.js
@@ -1,15 +1,13 @@
 ﻿$(function () {
 
-    var lng = "";
-    var lat = "";
-
-
+  
+    //baiduDingWei();
     gaodeDingWei();
     getDate();
 
     //获取当前登陆用户的用户名
     $.get("/Account/KqZSGetUserName",
-        function(data) {
+        function (data) {
 
             $("#username").html(data);
 
@@ -75,60 +73,57 @@
 
 //百度定位
 function baiduDingWei() {
-    var map = new BMap.Map("allmap");
-    var point = new BMap.Point(116.331398, 39.897445);
-    map.centerAndZoom(point, 15);
-    debugger;
+
+
+
+
+
     var geolocation = new BMap.Geolocation();
     geolocation.getCurrentPosition(function (r) {
-        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-            var mk = new BMap.Marker(r.point);
-            map.addOverlay(mk);
-            map.panTo(r.point);
+
+        var point = new BMap.Point(r.point.lng, r.point.lat);  //获取当前地理名称
+        var gc = new BMap.Geocoder();
+        gc.getLocation(point, function (rs) {
+            console.info(rs);
 
 
+            $("#location").html(rs.address + rs.business); 
 
-            lng = r.point.lng;
-            lat = r.point.lat;
-            var geocoder = new BMap.Geocoder();
-            geocoder.getLocation(r.point,
-                function (result) {
-                    abp.ui.clearBusy();
+            $("#location").attr("Remark", r.point.lng + "," + r.point.lat);
+        });
 
-
-                    $("#location").html(result.formattedAddress);
-                    $("#location").attr("Remark", lng + "," + lat);
-                });
-        }
-        else {
-            alert('failed' + this.getStatus());
-        }
     });
 
 }
 
+
+
+
 //高德定位
 function gaodeDingWei() {
-    var mapObj = new AMap.Map('iCenter');
-    mapObj.plugin('AMap.Geolocation', function () {
-        geolocation = new AMap.Geolocation({
-            enableHighAccuracy: true, // 是否使用高精度定位，默认:true
-            timeout: 10000,           // 超过10秒后停止定位，默认：无穷大
-            maximumAge: 0,            // 定位结果缓存0毫秒，默认：0
-            convert: true,            // 自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-            showButton: true,         // 显示定位按钮，默认：true
-            buttonPosition: 'LB',     // 定位按钮停靠位置，默认：'LB'，左下角
-            buttonOffset: new AMap.Pixel(10, 20), // 定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-            showMarker: true,         // 定位成功后在定位到的位置显示点标记，默认：true
-            showCircle: true,         // 定位成功后用圆圈表示定位精度范围，默认：true
-            panToLocation: true,      // 定位成功后将定位到的位置作为地图中心点，默认：true
-            zoomToAccuracy: true       // 定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+
+        var mapObj = new AMap.Map('iCenter');
+        mapObj.plugin('AMap.Geolocation', function () {
+            geolocation = new AMap.Geolocation({
+                enableHighAccuracy: true,//是否使用高精度定位，默认:true
+                timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+                maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+                convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+                showButton: true,        //显示定位按钮，默认：true
+                buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
+                buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+                showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
+                showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+                panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+                zoomToAccuracy: true ,     //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+                noGeoLocation: 3, //是否禁止使用IP定位，默认值为0，可选值0-3 ///0: 可以使用IP定位////1: 手机设备禁止使用IP定位///2: PC上禁止使用IP定位///3: 所有终端禁止使用IP定位
+          
+            });
+            geolocation.getCurrentPosition();
+            AMap.event.addListener(geolocation, 'complete', onComplete); // 返回定位信息
+            AMap.event.addListener(geolocation, 'error', onError);       // 返回定位出错信息
         });
-        mapObj.addControl(geolocation);
-        geolocation.getCurrentPosition();
-        AMap.event.addListener(geolocation, 'complete', onComplete); // 返回定位信息
-        AMap.event.addListener(geolocation, 'error', onError);       // 返回定位出错信息
-    });
+
 }
 
 function onComplete(obj) {
@@ -148,9 +143,15 @@ function onError(obj) {
     console.log(obj);
 }
 
+
+
+
 //获得当前日期
 function getDate() {
     var date = new Date();
     var nowdate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     $("#nowdate").html(nowdate);
 }
+
+
+
