9bed749666b705d58a7876c51d726ee18d0bc380
15086396171 <15086396171@163.com>
2019-02-21 15:51:35 +0800

    修改定位密钥

--- Vickn.Platform.Web/Views/WeiXingDK/Index_js.js
+++ Vickn.Platform.Web/Views/WeiXingDK/Index_js.js
@@ -116,14 +116,13 @@ function gaodeDingWei() {
                 showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
                 panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
                 zoomToAccuracy: true ,     //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-                noGeoLocation: 3, //是否禁止使用IP定位，默认值为0，可选值0-3 ///0: 可以使用IP定位////1: 手机设备禁止使用IP定位///2: PC上禁止使用IP定位///3: 所有终端禁止使用IP定位
+                //noGeoLocation: 3, //是否禁止使用IP定位，默认值为0，可选值0-3 ///0: 可以使用IP定位////1: 手机设备禁止使用IP定位///2: PC上禁止使用IP定位///3: 所有终端禁止使用IP定位
           
             });
             geolocation.getCurrentPosition();
             AMap.event.addListener(geolocation, 'complete', onComplete); // 返回定位信息
             AMap.event.addListener(geolocation, 'error', onError);       // 返回定位出错信息
-        });
-
+    });
 }
 
 function onComplete(obj) {
@@ -134,6 +133,7 @@ function onComplete(obj) {
         '\n地址：' + obj.formattedAddress +
         '\n地址信息：' + JSON.stringify(obj.addressComponent, null, 4);
     //alert(res);
+
     $("#location").html(obj.formattedAddress);
     $("#location").attr("Remark", obj.position);
 }
